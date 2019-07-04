132373caf73b08b1136b3c3c72d67204506966e6
祁美俊 <qimeijun@iworku.com>
2019-07-04 14:05:15 +0800

    将 element-ui css 源码提取出来，改造成我们需要的样式

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Element from 'element-ui'
-import '@/assets/style/element-variables.scss'
+// import '@/assets/style/element-variables.scss'
+import './../assets/style/source/theme-chalk/src/index.scss'
 
 
 Vue.use(Element)
